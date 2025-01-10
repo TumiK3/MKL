bbbc6f20ef2eb4e1d14be40aa4026e603dd7527e
TumiK3 <tumisholekga@gmail.com>
2025-01-10 13:50:05 +0200

    Create my_app.py

---
+++ my_app.py
@@ -0,0 +1,15 @@
+class MainWin(QWidget):
+  def __init__(self):
+    super().__init__()
+    self.set_appear()
+    self.initUI()
+    self.connects()
+    self.show()
+  def set_appear(self):
+    self.setWindowTitle(txt_title)
+    self.resize(win_width, win_height)
+    self.move(win_x, win_y)
+  def initUIself):
+    pass
+  def connects(self):
+    pass
