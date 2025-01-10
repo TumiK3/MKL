869224e4bf8b0e179bc4283a90340ceb3cd53242
TumiK3 <tumisholekga@gmail.com>
2025-01-10 14:27:53 +0200

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -1,3 +1,4 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QLineEdit
 class TestWin(QWidget):
   def __init__(self):
     super().__init__()
@@ -7,8 +8,17 @@ class TestWin(QWidget):
     self.show()
 
   def set_appear(self):
-    pass
+    self.setWindowTitle(txt_title)
+    self.resize(win_width, win_height)
+    self.move(win_x, win_y)
   def initUI(self):
-    pass
+    self.h_line = QHBoxLayout()
+    self.r_line = QVBoxLayout()
+    self.l_line = QVBoxLayout()
+    self.l_line.addWidget(self.btn1)
+    self.r_line.addWidget(self.timer)
+    self.h_line.addLayout(self.l_line)
+    self.h_line.addLayout(self.r_line)
+    self.setLayout(self.h_line)
   def connects(self):
     pass
