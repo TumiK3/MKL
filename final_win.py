fe982f9d5c8c0758fd52f39fd670dafbda722fd1
TumiK3 <tumisholekga@gmail.com>
2025-01-10 14:49:22 +0200

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -5,7 +5,23 @@ class FinalWin(QWidget):
     self.initUI()
     self.show()
 
-  def set_appear(self):
-    pass
+   def set_appear(self):
+    self.setWindowTitle(txt_title)
+    self.resize(win_width, win_height)
+    self.move(win_x, win_y)
+    
   def initUI(self):
-    pass
+    self.hello_text = QLabel(txt_hello)
+    self.instruction = QLabel(txt_instruction)
+    self.button = QPushButton(txt_next)
+    
+    self.layout = QVBoxLayout()
+    self.hello_text.addWidget(self.layout)
+    self.instruction.addWidget(self.layout)
+    self.button.addWidget(self.layout)
+    
+    self.setLayout(self.layout)
+
+app = QApplication([])
+fw = FinalWin()
+app.exec_()
