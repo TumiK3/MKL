09a6f1bbf809c3abbcf432f5812e0fdfb427c57c
TumiK3 <tumisholekga@gmail.com>
2025-01-10 14:00:08 +0200

    Update instr.py

--- instr.py
+++ instr.py
@@ -1,3 +1,7 @@
 tx_title = 'Health'
 win_x, win_y = 200, 100
 win_width, win_height = 1000, 600
+
+txt_hello = 'Welcome to the Health status detection program!'
+txt_instruction= 'This aplication allows you to use the Rufier test for...'
+txt_next = 'Start'
