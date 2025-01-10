7a261508a8a6efa30eb49a25741e5f9f6b8ee24d
TumiK3 <tumisholekga@gmail.com>
2025-01-10 14:46:51 +0200

    Create final_win.py

---
+++ final_win.py
@@ -0,0 +1,11 @@
+class FinalWin(QWidget):
+  def __init__(self):
+    super().__init__()
+    self.set_appear()
+    self.initUI()
+    self.show()
+
+  def set_appear(self):
+    pass
+  def initUI(self):
+    pass
