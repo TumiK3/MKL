6bad79a52cc83e9d035f0cbcf1317a114887d0e4
TumiK3 <tumisholekga@gmail.com>
2025-01-10 14:20:00 +0200

    Create second_win.py

---
+++ second_win.py
@@ -0,0 +1,14 @@
+class TestWin(QWidget):
+  def __init__(self):
+    super().__init__()
+    self.set_appear()
+    self.initUI()
+    self.connects()
+    self.show()
+
+  def set_appear(self):
+    pass
+  def initUI(self):
+    pass
+  def connects(self):
+    pass
