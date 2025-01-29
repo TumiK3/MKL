941cd9ffbe32766d296ed8783e659cee98e21895
TumiK3 <tumisholekga@gmail.com>
2025-01-29 14:32:03 +0200

    Update instr.py

--- instr.py
+++ instr.py
@@ -5,3 +5,11 @@ win_width, win_height = 1000, 600
 txt_hello = 'Welcome to the Health status detection program!'
 txt_instruction= 'This aplication allows you to use the Rufier test for...'
 txt_next = 'Start'
+
+txt_index = 'Roufier Index: '
+txt_workheart = 'Cardiac performance: '
+txt_res1 = "low. See your doctor right away!"
+txt_res2 = "satisfactory. See your doctor!"
+txt_res3 = "average. It may be worth seeing your doctor to get checked out."
+txt_res4 = "above average"
+txt_res5 = "high"
